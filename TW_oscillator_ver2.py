58974d79516512c6d000798e2d396e59d49245d2
theandychung <theandychung@gmail.com>
2017-04-08 01:12:31 +0800

    parameters updates

--- TW_oscillator_ver2.py
+++ TW_oscillator_ver2.py
@@ -1,52 +1,73 @@
+#<editor-fold desc="...import">
 import matplotlib.pyplot as plt
-import nengo
 from nengo.utils.functions import piecewise
+import matplotlib.pyplot as plt
+import numpy as np
+#</editor-fold>
+
+import nengo
 import math
 
-import numpy as np
-
-
-
-#initial values
-I = 0.11
-
-synapse=0.1;
-model = nengo.Network(label='WT Oscillator')
+I =.1
+epsilon=.2
+gamma=6.0
+beta=0.1
+tau=.1
+# I =-1
+# epsilon=.2
+# gamma=6.0
+# beta=0.1
+# tau=1
+model = nengo.Network(label='TW Oscillator')
 with model:
     inp = nengo.Node(I)
-    oscillator = nengo.Ensemble(850, dimensions=2, radius=20)
+    oscillator = nengo.Ensemble(150, dimensions=2,radius=1,
+                                neuron_type=nengo.LIFRate(tau_rc=0.02, tau_ref=0.002))
+                                #max_rates=[100])
      # osc to osc connection
     def feedback(x):
-        gamma = 2.1
-        beta = 0.1
-        eta = 0.35
-        return [synapse*(3 * x[0] - x[0]**3 + 2 - x[1])+x[0],
-                synapse*(eta * (gamma * (1 + math.tanh(x[0] / beta)) - x[1]))+x[1]]
-    nengo.Connection(oscillator, oscillator, synapse=synapse, function=feedback)
+        x,y = x
+        dx =  3 * x - x**3 + 2 - y
+        dy = epsilon * (gamma * (1 + math.tanh(x / beta)) - y)
+        return [tau*dx+x,tau*dy+y]
+    nengo.Connection(oscillator, oscillator, function=feedback, synapse=tau)
     # inp to osc connection
-    nengo.Connection(inp,oscillator[0], transform = synapse)
+    nengo.Connection(inp,oscillator[0], transform = [[tau]])
 
-################################################################################
-#probes
-    oscillatorx_pr = nengo.Probe(oscillator[0],synapse=synapse/10)
-    #oscillatory_pr = nengo.Probe(oscillator[1], synapse=tau / 10)
-    #inp_probe = nengo.Probe(inp,synapse = tau/10)
+############################################################################################
+    x_pr = nengo.Probe(oscillator[0],synapse=tau/10)
+    # xspikes_pr = nengo.Probe(oscillator.neurons,'spikes')
+    y_pr = nengo.Probe(oscillator[1], synapse=tau / 10)
 
-#run
+
 with nengo.Simulator(model) as sim:
-    sim.run(33)
+    sim.run(25)
 
-#plot
-import matplotlib.pyplot as plt
-plt.plot(sim.trange(),sim.data[oscillatorx_pr], label="Oscillator x output")
-#plt.plot(sim.trange(),sim.data[oscillator_yprobe], label="Oscillator y output")
 
-#plt.plot(sim.trange(), sim.data[inp_probe], 'r', label="Input")
-plt.title("Time Domain")
-plt.xlabel("Time (s)")
-plt.ylabel("x activity")
-plt.ylim(-5, 5);
-plt.legend();
+
+#<editor-fold desc="...plot">
+t = sim.trange()
+F1 = plt.figure(figsize=(10,5))
+f11 = F1.add_subplot(1,2,1)
+f11.plot(t,sim.data[x_pr], label="x activity")
+f11.plot(t,sim.data[y_pr], label="y activity")
+f11.set_title("Activities of the Oscillator")
+f11.set_xlabel("Time (s)")
+f11.set_ylabel("x&y activities")
+f11.set_ylim(-3, 7)
+f11.legend()
+
+f22 = F1.add_subplot(1,2,2)
+f22.plot(sim.data[x_pr],sim.data[y_pr], label="phase plane")
+f22.set_xlabel("x activity")
+f22.set_ylabel("y activity")
+f22.set_ylim(-2, 10)
+f22.set_xlim(-3, 3)
+#
+# F2 = plt.figure()
+# plt.plot(t,sim.data[xspikes_pr])
+
 plt.show()
 #prettify()
 #plt.legend(['$x_0$', '$x_1$', '$\omega$']);
+#</editor-fold>
